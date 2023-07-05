db549872dc3cfa7e14616d3fa01b98633d2ffc7a
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-07-05 15:09:18 -0300

    Se creo un modelo de datos para las reservas

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-const express = require('express');
-const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 require('dotenv').config();
@@ -15,6 +13,11 @@ const port = process.env.PORT || 4500
 
 // Middlewares
 // TODO: Implementar middlewares
+//app.use(helmet())
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    return res.status(404).render('404');
+})
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`El servidor se encuentra en http://localhost:${port}`));
--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,80 @@
 // TODO: Crear modelo de datos de Reserva
+const { DataTypes, sequelize } = require('../database');
+
+const Reserva = sequelize.define('Reserva', {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    codigo: {
+        type: DataTypes.STRING(100),
+
+        allowNull: false,
+        unique: true,
+        defaultValue: new Date().getTime()
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    destino: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    fecha_vuelo: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    nombre_aereolinea: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    cantidad_acompañantes: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    telefono: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'reservas'
+});
+
+// Si la tabla no existe cambia ({force: true} borra y crea la tabla)
+Reserva.sync({ force: false }).then(() => {
+    console.log('Se creó la tabla :)');
+});
+
+module.exports = Reserva;
